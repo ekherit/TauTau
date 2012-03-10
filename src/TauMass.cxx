4c895bc8e1d30f210e43e46713b719f5cef454cc
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2012-03-11 05:12:24 +0700

    Select events with good electron tracks.

--- src/TauMass.cxx
+++ src/TauMass.cxx
@@ -941,7 +941,8 @@ StatusCode TauMass::execute()
     if(tau_sig) tau_events++;
 
     //if(!tau_sig && !bhabha_sig) goto SKIP_CHARGED;  
-    if(!tau_sig && STRICT_TAU_CUT == 1) goto SKIP_CHARGED;  
+    //if(!tau_sig && STRICT_TAU_CUT == 1) goto SKIP_CHARGED;  
+    if(! (ise[0] || ise[1] ) && STRICT_TAU_CUT == 1) goto SKIP_CHARGED;  
 
     /* now fill the data */
     main_tuple->write();
