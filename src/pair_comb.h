67ca1489d13b2d0ad82180ca35b35eb9ebb471ae
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2018-12-18 10:34:24 +0700

    pairs combination

--- src/pair_comb.h
+++ src/pair_comb.h
@@ -125,3 +125,102 @@ void test_make_combination_list2(int N=3)
     if(m->second!=1) std::cout << "ERROR wrong combination entry" << std::endl;
   }
 }
+#include <vector>
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <iomanip>
+
+template< class Ps>
+void print_pairs( const Ps & P)
+{
+  for( const auto & p: P)
+  {
+    std::cout << p.first<<p.second<< " ";
+  }
+};
+template< class Cs>
+void print( const Cs & C)
+{
+  int idx=1;
+  for(const auto & P: C)
+  {
+    std::cout << std::setw(20) << idx << "  : ";
+    print_pairs(P);
+    std::cout << std::endl;
+    idx++;
+  }
+};
+
+template
+< 
+    class ItA,   //iterator in the first container of objects type A
+    class ItB,   //iterator in the second container of object type B
+    class R      //result contains all combinations of pairs:  
+    //   { (a1,b1) (a2,b2) (a3,b3) }
+    //   { (a1,b2) (a2,b1) (a3,b3) }
+    //   ...
+>
+void  make_unique_pairs
+(
+   ItA a_begin, ItA a_end, 
+   ItB b_begin, ItB b_end, 
+   R & result
+)
+{
+  //this is the type of pair's items (A,B)
+  using A = typename std::iterator_traits<ItA>::value_type; 
+  using B = typename std::iterator_traits<ItB>::value_type;
+  //the end of recursion
+  if( a_begin==a_end || b_begin==b_end) return;
+  if( result.empty() ) result.push_back( typename R::value_type() ); //initial start
+  typename R::value_type begin_combination; 
+  std::copy(result.back().begin(),result.back().end(), std::back_inserter(begin_combination));
+  for(auto a = a_begin; a!=a_end; ++a)
+  {
+    if(a != a_begin) std::swap(*a, *a_begin);
+    for(auto b = b_begin; b!=b_end; ++b)
+    {
+      if(b!= b_begin) std::swap(*b,*b_begin);
+      R tmp{typename R::value_type({std::pair<A,B> (*a_begin, *b_begin)})};
+      make_unique_pairs(next(a_begin), a_end, next(b_begin), b_end, tmp);
+      for(auto it = tmp.begin(); it!=tmp.end(); ++it)
+      {
+        std::copy(it->begin(), it->end(),std::back_inserter(result.back()));
+        if(std::next(a) != a_end || std::next(b) != b_end || std::next(it)!=tmp.end()) result.push_back(begin_combination); 
+      }
+      if(b!= b_begin) std::swap(*b,*b_begin);//swap back
+    }
+    if(a != a_begin) std::swap(*a, *a_begin); //swap back
+  }
+  for(auto & r : result) std::sort(r.begin(),r.end());
+  std::sort(result.begin(),result.end());
+  result.erase(std::unique(result.begin(),result.end()), result.end());
+}
+
+inline void test_make_pairs(int NA=1, int NB=1)
+{
+  std::vector<int>  A={1};
+  std::vector<char> B={'a'};
+  for( int i=1;i<NA;++i) A.push_back(i+1);
+  for( int i=1;i<NB;++i) B.push_back('a'+i);
+  auto print_array = [](std::string name="", const auto & A)
+  {
+    std::cout << name << " = {";
+    for(auto it = A.begin(); it!=A.end(); ++it)
+    {
+      std::cout << *it;
+      if( std::next(it)!=A.end() ) std::cout << ',';
+    }
+    std::cout << "}\n";
+  };
+  print_array("A",A);
+  print_array("B",B);
+  std::vector< std::vector < std::pair<int, char> > > R;
+  make_unique_pairs(A.begin(), A.end(), B.begin(), B.end(), R);
+  std::cout << "The result of algorithm: " << R.size() << " combinations:" << std::endl;
+  print(R);
+}
+
+
+
