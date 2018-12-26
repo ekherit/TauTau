6a10c586869b0b41a648ab8540e17377f2c055c4
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2018-12-26 11:55:10 +0700

    Fixing compilation bug

--- src/TauTau.cxx
+++ src/TauTau.cxx
@@ -288,9 +288,10 @@ StatusCode TauTau::execute()
     //create combination list
     typedef std::list < std::pair<HepLorentzVector*, HepLorentzVector*> > comb_t;
     typedef std::vector< comb_t > comb_list_t;
+    //std::vector<HepLorentzVector*> 
     comb_list_t pi0_cmb_list;
-    make_unique_pairs(Pn.begin(),Pn.end(),pi0_cmb_list);
-    //std::vector< std::list < std::pair<HepLorentzVector*, HepLorentzVector*> > > pi0_cmb_list = make_combination_list(Pn); 
+    //make_unique_pairs(Pn.begin(),Pn.end(),pi0_cmb_list);
+    comb_list_t pi0_cmb_list = make_combination_list(Pn); 
     //loop over all combinations
     comb_list_t::iterator best_comb=pi0_cmb_list.begin();
     double chi2_mass=1e100;
