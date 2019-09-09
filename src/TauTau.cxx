88f0780478f5538b778ad3f95a4f49e9c38a4713
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-09-09 20:17:27 +0700

    fix bug in calculating good_pi0

--- src/TauTau.cxx
+++ src/TauTau.cxx
@@ -324,7 +324,7 @@ StatusCode TauTau::execute()
         {
           double m = (*(it_pair->first) +  *(it_pair->second)).mag(); //again calculate the pi0 mass
           fEvent.Mpi0[idx] = m;
-          select = select || ( fabs(m - PI0_MASS) <  0.03); //selection of the pi0
+          has_good_pi0 = has_good_pi0 || ( fabs(m - PI0_MASS) <  0.03); //selection of the pi0
           //now create all combination to tie pi0 with charged tracks
           for(int i = 0; i<Tc.size(); ++i)
           {
