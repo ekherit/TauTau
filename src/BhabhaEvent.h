f76b86de4864f63ee63268a4128b665b2729903e
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-11-12 19:36:37 +0700

    Fixing compilation bugs

--- src/BhabhaEvent.h
+++ src/BhabhaEvent.h
@@ -80,8 +80,8 @@ class BhabhaEvent : public RootTuple
       tuple->addItem ("run", run);
       tuple->addItem ("event", event);
       tuple->addItem ("time", time);
-      tuple->addItem ("N0", N0, 0,MAX_NEUTRAL_TRACKS_NUMBER); 
-      tuple->addItem ("Nq", Nq, 0,MAX_CHARGED_TRACKS_NUMBER); 
+      tuple->addItem ("N0", N0, 0,3); 
+      tuple->addItem ("Nq", Nq, 0,3); 
       tuple->addItem ("dphi", delta_phi); 
       tuple->addItem ("dtheta", delta_theta); 
       tuple->addItem("acol",acol);
