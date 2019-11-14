652edcd1b47caeab7e02c3aa9e133e4dd55d3a99
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-11-14 16:20:59 +0700

    make job options based on scan point table

--- scripts/ScanPoint.py
+++ scripts/ScanPoint.py
@@ -1,6 +1,6 @@
 #!/bin/python
 import re
-
+import copy
 # ScanPoint class hold information about scan point: name, energy, energy spread, luminocity and
 # runlist
 class ScanPoint:
@@ -9,12 +9,13 @@ class ScanPoint:
     dW = 0  # energy error, GeV
     Sw = 0  # energy spread, MeV
     dSw = 0 # energy spread error, MeV
-    L = 0   # luminocity
+    L  = 0  # luminosity value, pb^-1
+    dL = 0  # luminosity error, pb^-1
     runlist = []  #list of the runs
 
-    def print_head(self):
-        s = "%5s %10s %10s %10s %10s %10s %s" % ("#name","W,GeV", "dW", "Sw", "dSw", "L,pb^-1", "   runs")
-        print s
+    def head(self):
+        return "%5s %10s %10s %10s %10s %10s %10s %3s %-10s" % ("#name","W,GeV", "dW,GeV", "Sw,GeV", "dSw,GeV", "L,pb^-1", "dL,pb^-1", "", "runs")
+
 
 #make string with folder runlist
     def fold(self, rl):
@@ -29,17 +30,29 @@ class ScanPoint:
         return s
 
 #print point ifno
+    def str(self):
+        s  = "%5s %10.6f %10.6f %10.6f %10.6f %10.6f %10.6f %3s %-10s" % (self.title, self.W, self.dW, self.Sw, self.dSw, self.L, self.dL, "", self.fold(self.runlist))
+        return s
+
+    def print_head(self):
+        print self.head()
+
     def prn(self):
-        s = "%5s %10.6f %10.6f %10.3f %10.3f %10.3f" % (self.title, self.W, self.dW, self.Sw, self.dSw, self.L)
-        print s,"  ",self.fold(self.runlist)
+        print self.str()
+
 
 
 def read_scan_point_table(cfg_file_name):
     cfg_file = open(cfg_file_name, "r")
     Scan = []
+    empty_re = re.compile("^\s*$")
+    comment_re = re.compile("^\s*#.*")
+    run_range_re=re.compile("^\s*(\d+)\s*-\s*(\d+)")
+    single_run_re=re.compile("^\s*\d+")
     for line in cfg_file:
+        if re.match(empty_re,line)   : continue
+        if re.match(comment_re,line) : continue
         cols = line.split()
-        if cols[0] == '#': continue
         sp = ScanPoint()
         sp.title = cols[0];
         sp.W =   float(cols[1]);
@@ -47,10 +60,9 @@ def read_scan_point_table(cfg_file_name):
         sp.Sw =  float(cols[3]);
         sp.dSw = float(cols[4]);
         sp.L =   float(cols[5]);
-        run_range_re=re.compile("^\s*(\d+)\s*-\s*(\d+)")
-        single_run_re=re.compile("^\s*\d+")
+        sp.dL =   float(cols[6]);
         runlist = []
-        for l in range(6, len(cols)):
+        for l in range(7, len(cols)):
             r = re.match(run_range_re,cols[l])
             if r:
                 for run in range(int(r.group(1)), int(r.group(2))+1):
@@ -67,4 +79,85 @@ def test():
     Scan[0].print_head();
     for sp in Scan:
         sp.prn()
+
+
+
+
+class RunInfo:
+    title = ''
+    run = 0
+    L = 0.0
+
+
+def read_run_info(filename):
+    f = open(filename, "r")
+    RI = []
+    empty_re = re.compile("^\s*$")
+    comment_re = re.compile("^\s*#.*")
+    not_number_re = re.compile("^\s*[^\d].*")
+    for line in f:
+        if re.match(empty_re,line)   : continue
+        if re.match(comment_re,line) : continue
+        if re.match(not_number_re,line) : continue
+        cols = line.split()
+        ri = RunInfo()
+        #print cols[0], cols[1]
+        ri.run = int(cols[0])
+        ri.L   = float(cols[1])*1e-3
+        RI.append(ri)
+    return RI
+
+
+def calculate_luminosity_from_runinfo(S, RI):
+    result = []
+    for point in S:
+        p = copy.copy(point)
+        p.L = 0
+        p.dL = point.dL
+        for run in point.runlist:
+            b = filter(lambda x: x.run == run, RI)
+            if len(b) >1:
+                print "WARNING: duplicate runinfo for run: ",run
+            if len(b) == 1:
+                p.L += b[0].L
+        result.append(p)
+    return result
+
+
+
+def make_runinfo_lum(scanpoint_file, runinfo_file):
+    Scan = read_scan_point_table(scanpoint_file)
+    print "Original file: "
+    Scan[0].print_head();
+    for sp in Scan:
+        sp.prn()
+    RI = read_run_info(runinfo_file)
+    ScanNew = calculate_luminosity_from_runinfo(Scan, RI)
+    print "New file: "
+    Scan[0].print_head();
+    for sp in ScanNew:
+        sp.prn()
+
+    print ""
+    print "Difference: "
+    for i in range(0,len(Scan)):
+        print "%10s %10.6f %10.6f %10.6f %10.6f" % (Scan[i].title, Scan[i].W, Scan[i].L, ScanNew[i].L,  (Scan[i].L-ScanNew[i].L))
+    return ScanNew
+
+def write_to_file(Scan, filename):
+    f = open(filename,"w")
+    if len(Scan)>0:
+        f.write(Scan[0].head()+"\n")
+    for point in Scan:
+        f.write(point.str()+"\n")
+    f.close()
+
+
+
+#ScanNew = make_runinfo_lum("all_scan_points_ems3.txt", "../share/tau_scan_run_info_55060_55361.txt");
+#write_to_file(ScanNew,"new.txt")
+
+
+
+
 
