62ce4d7a82f70a816838dcc68d3089e80ce5e63c
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-11-14 21:56:31 +0700

    Add digamma

--- scripts/make_all_jobs.sh
+++ scripts/make_all_jobs.sh
@@ -5,12 +5,17 @@ cd $WDIR
 TDIR=$WDIR/$1
 mkdir $TDIR
 cd $TDIR
-mkdir data
-cd data
-$TAUTAUROOT/scripts/make_job_option.py $WDIR/data --config=$TAUTAUROOT/share/all_scan_points_ems3.txt 
-cd ../
+#mkdir data
+#cd data
+$TAUTAUROOT/scripts/make_job_option.py $WDIR/data data --config=$TAUTAUROOT/share/all_scan_points_ems3.txt 
+#cd ../
 mkdir mc
-cd mc
-mkdir bhabha
-cd bhabha
-$TAUTAUROOT/scripts/make_job_option.py /besfs/groups/tauqcd/zhangjy/bos704/MC/lumi/bhabha bb --W=1 --combine='\d\.\d+'
+#cd mc
+#mkdir bhabha
+#cd bhabha
+$TAUTAUROOT/scripts/make_job_option.py /besfs/groups/tauqcd/zhangjy/bos704/MC/lumi/bhabha mc/bb --W=1 --combine='\d\.\d+'
+#cd ../
+#mkdir gg
+#cd gg
+$TAUTAUROOT/scripts/make_job_option.py /besfs/groups/tauqcd/zhangjy/bos704/MC/lumi/digam mc/gg --W=1 --combine='\d\.\d+'
+
