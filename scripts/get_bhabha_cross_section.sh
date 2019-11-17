a019cc6bd021d2d81a6357d92fc3624308d71b29
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-11-17 13:28:30 +0700

    Add finding gg cross section

--- scripts/get_bhabha_cross_section.sh
+++ scripts/get_bhabha_cross_section.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#run it in monte carlo with hadrons
-cd /besfs/groups/tauqcd/zhangjy/bos704/subjob/PubSimRec/lumi/bhabha
-#!/bin/bash
 cd /besfs/groups/tauqcd/zhangjy/bos704/subjob/PubSimRec/lumi/bhabha
 for i in 3.* 
 do 
