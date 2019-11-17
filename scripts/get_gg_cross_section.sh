bb1b392896108da6211b95ab85dc8cd521c2a827
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-11-17 13:31:29 +0700

    Digamma cross

---
+++ scripts/get_gg_cross_section.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /besfs/groups/tauqcd/zhangjy/bos704/subjob/PubSimRec/lumi/digam
+for i in 3.* 
+do 
+  sigma="`cat $i/CrossSection.txt | tail -2 | head -1`"
+  echo $i "      " $sigma
+done
