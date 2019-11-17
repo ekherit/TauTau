0964038b45d0ec6bf6c629baadd9734fcbaadb6d
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-11-17 13:23:14 +0700

    fixing bug

--- scripts/get_bhabha_cross_section.h
+++ scripts/get_bhabha_cross_section.h
@@ -6,5 +6,5 @@ cd /besfs/groups/tauqcd/zhangjy/bos704/subjob/PubSimRec/lumi/bhabha
 for i in 3.* 
 do 
   sigma="`cat $i/CrossSection.txt | tail -2 | head -1`"
-  echo $i   sigma
+  echo $i   $sigma
 done
