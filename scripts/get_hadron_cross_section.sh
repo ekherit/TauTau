9198e93c237aceb0b366e3d59b8ac29874e95b5b
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-12-26 15:32:39 +0700

    enery and cross section in one row

--- scripts/get_hadron_cross_section.sh
+++ scripts/get_hadron_cross_section.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #run it in monte carlo with hadrons
 cd /besfs/groups/tauqcd/zhangjy/bos704/subjob/PubSimRec/hadrons
-for i in 3.*; do echo W=$i; cat $i/obsxs.dat | awk '{a=a+$2;}; END {print "sigma=",a}'; done;
+for i in 3.*; do echo -n $i; cat $i/obsxs.dat | awk '{a=a+$2;}; END {print "sigma=",a}'; done;
 
