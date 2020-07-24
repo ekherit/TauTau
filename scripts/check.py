86b4be2a8dc13b16c47da87b32f2ecc628a89855
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2020-07-24 19:54:37 +0700

    Add file size

--- scripts/check.py
+++ scripts/check.py
@@ -8,25 +8,32 @@ re_file_pattern = ".+.cfg$"
 r = re.compile("Event proceed:")
 okfile = open("OK.txt", "w")
 failfile = open("FAIL.txt", "w")
+fmt = "%-50s %20s %20s %10.3f Mb %10s\n" 
 for subdir, dirs, files in os.walk(dir):
     for f in files:
         count += 1
         if re.match(re_file_pattern, f):
-            print(f)
             fname = os.path.join(subdir, f)
             fname_boss = fname+".bosslog"
-            match = False
+            base_fname = os.path.splitext(fname)[0]  
+            root_fname = base_fname + ".root"
             print("Checking %s  %s" % (fname, fname_boss))
-            try:
-                file = open(fname_boss,"r");
-                for line in file.readlines():
-                    if(re.match(r, line)):
-                        match = True
-                        okfile.write("%-50s %10s\n" % (fname, "OK"))
-                        break
-                file.close()
+            try :
+                st = os.stat(root_fname)
+                file_size =  st.st_size/1024/1024 #in Mb
+                ok = False
+                try:
+                    file = open(fname_boss,"r");
+                    for line in file.readlines():
+                        if(re.match(r, line)):
+                            ok = True
+                            break
+                    file.close()
             except:
-                match = False
-            if not match:
-                failfile.write("%-50s %10s\n" % (fname, "FAIL"))
+                ok = False
+
+            if ok:
+                okfile.write(fmt % (fname, fname_boss, root_fname, file_size, "OK"))
+            else:
+                failfile.write(fmt % (fname, fname_boss, root_fname, file_size, "OK"))
 
