c233a35d0e4d73c923edf7ec8b224b394a27ff3d
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2020-07-24 20:01:36 +0700

    Fix bug

--- scripts/check.py
+++ scripts/check.py
@@ -8,7 +8,7 @@ re_file_pattern = ".+.cfg$"
 r = re.compile("Event proceed:")
 okfile = open("OK.txt", "w")
 failfile = open("FAIL.txt", "w")
-fmt = "%-50s %20s %20s %10.3f Mb %10s\n" 
+fmt = "%-50s %50s %50s %10.3f Mb %10s\n" 
 for subdir, dirs, files in os.walk(dir):
     for f in files:
         count += 1
@@ -18,22 +18,21 @@ for subdir, dirs, files in os.walk(dir):
             base_fname = os.path.splitext(fname)[0]  
             root_fname = base_fname + ".root"
             print("Checking %s  %s" % (fname, fname_boss))
+            ok = False
+            file_size = 0
             try :
                 st = os.stat(root_fname)
                 file_size =  st.st_size/1024/1024 #in Mb
-                ok = False
-                try:
-                    file = open(fname_boss,"r");
-                    for line in file.readlines():
-                        if(re.match(r, line)):
-                            ok = True
-                            break
-                    file.close()
+                file = open(fname_boss,"r");
+                for line in file.readlines():
+                    if(re.match(r, line)):
+                        ok = file_size > 0.1
+                        break
+                file.close()
             except:
                 ok = False
-
             if ok:
                 okfile.write(fmt % (fname, fname_boss, root_fname, file_size, "OK"))
             else:
-                failfile.write(fmt % (fname, fname_boss, root_fname, file_size, "OK"))
+                failfile.write(fmt % (fname, fname_boss, root_fname, file_size, "FAIL"))
 
